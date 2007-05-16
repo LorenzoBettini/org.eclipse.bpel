388a94da3611ae017ee7ad751d4250debf17979c
mchmielewski <mchmielewski>
2007-05-16 18:00:17 +0000

    Added decorated label, which is the same as CLabel, however, CLabel does not expose any way to control margins.

--- plugins/org.eclipse.bpel.common.ui/src/org/eclipse/bpel/common/ui/details/widgets/StatusLabel2.java
+++ plugins/org.eclipse.bpel.common.ui/src/org/eclipse/bpel/common/ui/details/widgets/StatusLabel2.java
@@ -17,20 +17,27 @@ import org.eclipse.bpel.common.ui.CommonUIPlugin;
 import org.eclipse.bpel.common.ui.ICommonUIConstants;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.jface.resource.ImageRegistry;
-import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Control;
+
+/**
+ * Status label which holds information from text markers next to labels.
+ * 
+ * @author IBM
+ * @author Michal Chmielewski (michal.chmielewski@oracle.com)
+ * @date May 15, 2007
+ *
+ */
 
 public class StatusLabel2 {
 
 	final private int MAX_MESSAGES = 5;
 	final private String STATUS_MESSAGE_ID = "STATUS_MESSAGE"; //$NON-NLS-1$
-	final private String EMPTY_STRING = ""; //$NON-NLS-1$
 	final private String NEW_LINE = "\n"; //$NON-NLS-1$
 	final private String ETCETERA = "..."; //$NON-NLS-1$
 
-	protected CLabel label;
-	protected List statusMessageList;
+	protected DecoratedLabel label;
+	protected List<StatusMessage> statusMessageList;
 
 	protected static Image blankImage, infoImage, warnImage, errorImage;
 
@@ -48,8 +55,8 @@ public class StatusLabel2 {
 		 * constructor
 		 * 
 		 */
-		protected StatusMessage(String messageId, int severity, String message) {
-			setSeverity(messageId, severity, message);
+		protected StatusMessage(String messageId, int sev, String msg) {
+			setSeverity(messageId, sev, msg);
 		}
 
 		/**
@@ -63,14 +70,15 @@ public class StatusLabel2 {
 		/**
 		 * Sets the severity and (optional) message.
 		 * 
+		 * @param messageId message id
 		 * @param sev One of IStatus.OK, IStatus.INFO, IStatus.WARNING, IStatus.ERROR
-		 * @param message A text message describing the condition (or null).
+		 * @param msg A text message describing the condition (or null).
 		 * 
 		 */
-		protected void setSeverity(String messageId, int severity, String message) {
+		protected void setSeverity(String messageId, int sev, String msg) {
 			setId(messageId);
-			setSeverity(severity);
-			setMessage(message);
+			setSeverity(sev);
+			setMessage(msg);
 		}
 
 		/**
@@ -78,7 +86,7 @@ public class StatusLabel2 {
 		 * 
 		 * @param IStatus
 		 */
-		protected void setStatus(IStatus status) {
+		protected void setStatus (IStatus status) {
 			if (status == null) {
 				setSeverity(STATUS_MESSAGE_ID, IStatus.OK, null);
 			} else {
@@ -87,14 +95,14 @@ public class StatusLabel2 {
 		}
 
 		/**
-		 * @return int the severity
+		 * @return the severity
 		 */
 		protected int getSeverity() {
 			return severity;
 		}
 
 		/**
-		 * @param int the severity
+		 * @param i the severity
 		 */
 		protected void setSeverity(int i) {
 			severity = i;
@@ -132,17 +140,18 @@ public class StatusLabel2 {
 	/**
 	 * constructor
 	 * 
-	 * @param CLabel to be wrapped
+	 * @param aLabel to be wrapped
 	 * 
 	 */
-	public StatusLabel2(CLabel label) {
-		this.label = label;
+	
+	public StatusLabel2(DecoratedLabel aLabel) {
+		this.label = aLabel;
 
 		if (blankImage == null) {
 			initSharedImages();
 		}
 
-		setStatusMessageList(new ArrayList());
+		setStatusMessageList(new ArrayList<StatusMessage>());
 		
 		clear();
 	}
@@ -153,7 +162,8 @@ public class StatusLabel2 {
 	 */
 	protected static void initSharedImages() {
 		ImageRegistry registry = CommonUIPlugin.getDefault().getImageRegistry();
-		blankImage = registry.get(ICommonUIConstants.ICON_SM_BLANK);
+		// blankImage = registry.get(ICommonUIConstants.ICON_SM_BLANK);
+		
 		infoImage = registry.get(ICommonUIConstants.ICON_SM_INFO);
 		warnImage = registry.get(ICommonUIConstants.ICON_SM_WARN);
 		errorImage = registry.get(ICommonUIConstants.ICON_SM_ERROR);
@@ -165,7 +175,7 @@ public class StatusLabel2 {
 	 * @return CLabel
 	 * 
 	 */
-	public CLabel getLabel() {
+	public DecoratedLabel getLabel() {
 		return label;
 	}
 
@@ -192,7 +202,7 @@ public class StatusLabel2 {
 	/**
 	 * Sets the CLabel text
 	 * 
-	 * @param String
+	 * @param s the label text
 	 * 
 	 */
 	public void setText(String s) {
@@ -210,9 +220,9 @@ public class StatusLabel2 {
 	}
 
 	/**
-	 * Sets the CLabel text
+	 * Sets the CLabel LayoutData
 	 * 
-	 * @return Object LayoutData
+	 * @param layoutData layout data
 	 * 
 	 */
 	public void setLayoutData(Object layoutData) {
@@ -224,63 +234,53 @@ public class StatusLabel2 {
 	 * 
 	 */
 	public void clear() {
-		getStatusMessageList().clear();
+		statusMessageList.clear();
 		updateLabel() ;
 	}
 
 	/**
 	 * clears all status message with a id equals to the passed messageId
 	 * 
-	 * @param String id of status message to be removed
+	 * @param messageId id of status message to be removed
 	 * 
 	 */
-	public void clear(String messageId) {
+	public void clear (String messageId) {
 
 		if (messageId == null) {
 			clear();
-
-		} else {
-			List removeList = new ArrayList();
-
-			// get all status messages to be removed
-			for (int i = 0; i < getStatusMessageList().size(); i++) {
-				StatusMessage statusMessage = (StatusMessage) getStatusMessageList().get(i);
-
-				if (messageId.equals(statusMessage.getId())) {
-					removeList.add(statusMessage);
-				}
-			}
-
-			// remove all just now collected status messages
-			for (int i = 0; i < removeList.size(); i++) {
-				getStatusMessageList().remove(removeList.get(i));
-			}
-			updateLabel() ;
+			return ;
 		}
+		List<StatusMessage> removeList = new ArrayList<StatusMessage>();
+
+		// get all status messages to be removed
+		for(StatusMessage m : statusMessageList ) {
+			if (messageId.equals(m.getId())) {
+				removeList.add(m);
+			}
+		}
+		// remove all just now collected status messages
+		statusMessageList.removeAll(removeList);
+		// update label
+		updateLabel() ;
 	}
 
 	/**
 	 * Sets the severity and (optional) message.
 	 * 
-	 * @param String id of status message
-	 * @param int severity. One of IStatus.OK, IStatus.INFO, IStatus.WARNING, IStatus.ERROR
-	 * @param String message. A text message describing the condition (or null).
+	 * @param messageId id of status message
+	 * @param severity  One of IStatus.OK, IStatus.INFO, IStatus.WARNING, IStatus.ERROR
+	 * @param message A text message describing the condition (or null).
 	 * 
 	 */
 	public void setSeverity(String messageId, int severity, String message) {
-
-		StatusMessage statusMessage = new StatusMessage(messageId, severity, message);
-
-		getStatusMessageList().clear();
-		getStatusMessageList().add(statusMessage);
-
-		updateLabel();
+		statusMessageList.clear();
+		addSeverity(severity,message);
 	}
 
 	/**
 	 * Sets the severity and (optional) message. The status message Id will be set to default
 	 * 
-	 * @param sev One of IStatus.OK, IStatus.INFO, IStatus.WARNING, IStatus.ERROR
+	 * @param severity One of IStatus.OK, IStatus.INFO, IStatus.WARNING, IStatus.ERROR
 	 * @param message A text message describing the condition (or null).
 	 * 
 	 */
@@ -291,7 +291,7 @@ public class StatusLabel2 {
 	/**
 	 * Convenience method taking an IStatus as input.
 	 * 
-	 * @param IStatus
+	 * @param status the status to set
 	 * 
 	 */
 	public void setStatus(IStatus status) {
@@ -302,27 +302,24 @@ public class StatusLabel2 {
 		}
 	}
 
+	
 	/**
 	 * Sets the severity and (optional) message.
 	 *
-	 * @param String id of status message  
-	 * @param sev One of IStatus.OK, IStatus.INFO, IStatus.WARNING, IStatus.ERROR
+	 * @param messageId id of status message  
+	 * @param severity One of IStatus.OK, IStatus.INFO, IStatus.WARNING, IStatus.ERROR
 	 * @param message A text message describing the condition (or null).
 	 * 
 	 */
-	public void addSeverity(String messageId, int severity, String message) {
-
-		StatusMessage statusMessage = new StatusMessage(messageId, severity, message);
-
-		getStatusMessageList().add(statusMessage);
-
+	public void addSeverity(String messageId, int severity, String message) {			
+		statusMessageList.add(new StatusMessage(messageId, severity, message) );
 		updateLabel();
 	}
 
 	/**
 	 * Sets the severity and (optional) message. The status message Id will be set to default
 	 * 
-	 * @param sev One of IStatus.OK, IStatus.INFO, IStatus.WARNING, IStatus.ERROR
+	 * @param severity One of IStatus.OK, IStatus.INFO, IStatus.WARNING, IStatus.ERROR
 	 * @param message A text message describing the condition (or null).
 	 * 
 	 */
@@ -332,13 +329,11 @@ public class StatusLabel2 {
 
 	/**
 	 * Convenience method taking an IStatus as input.
-	 * 
-	 * @param IStatus
+	 * @param status  
 	 */
-	public void addStatus(IStatus status) {
-		if (status == null) {
-			addSeverity(IStatus.OK, null);
-		} else {
+	
+	public void addStatus (IStatus status) {
+		if (status != null) {
 			addSeverity(status.getSeverity(), status.getMessage());
 		}
 	}
@@ -365,7 +360,8 @@ public class StatusLabel2 {
 
 	/**
 	 * returns the tool tip text. Only a maximum of messages will be appended to the tool tip text. The different
-	 * messages are seperated with a new line charater. If there are more messages than the maximum number, than three
+	 * messages are separated with a new line character. 
+	 * If there are more messages than the maximum number, than three
 	 * dots will be appended to the tool tip text.
 	 * 
 	 * @return the tool tip text from the status message list
@@ -373,80 +369,63 @@ public class StatusLabel2 {
 	 */
 	protected String getToolTipText() {
 
-		String toolTipText = EMPTY_STRING;
-		List toolTipTextList = new ArrayList();
+		StringBuilder toolTipText = new StringBuilder(256);		
+		List<String> toolTipTextList = new ArrayList<String>();
 
-		toolTipTextList = appendToolTipTextList(toolTipTextList, IStatus.ERROR, MAX_MESSAGES);
-		toolTipTextList = appendToolTipTextList(toolTipTextList, IStatus.WARNING, MAX_MESSAGES);
-		toolTipTextList = appendToolTipTextList(toolTipTextList, IStatus.INFO, MAX_MESSAGES);
-		toolTipTextList = appendToolTipTextList(toolTipTextList, IStatus.OK, MAX_MESSAGES);
+		appendToolTipTextList(toolTipTextList, IStatus.ERROR, MAX_MESSAGES);
+		appendToolTipTextList(toolTipTextList, IStatus.WARNING, MAX_MESSAGES);
+		appendToolTipTextList(toolTipTextList, IStatus.INFO, MAX_MESSAGES);
+		appendToolTipTextList(toolTipTextList, IStatus.OK, MAX_MESSAGES);
 
-		for (int i = 0; i < toolTipTextList.size(); i++) {
-			if (toolTipText != null && toolTipText.length() > 0) {
-				toolTipText += NEW_LINE;
+		for (String t : toolTipTextList) {
+			toolTipText.append(t).append(NEW_LINE);
+		}
+		
+		if (statusMessageList.size() > toolTipTextList.size()) {
+			if (toolTipText.length() > 0) {
+				toolTipText.append(ETCETERA);
 			}
-			toolTipText += toolTipTextList.get(i);
 		}
 
-		if (getStatusMessageList().size() > toolTipTextList.size()) {
-			if (toolTipText != null && toolTipText.length() > 0) {
-				toolTipText += NEW_LINE;
-			}
-			toolTipText += ETCETERA;
-		}
-
-		return toolTipText;
+		return toolTipText.toString().trim();
 	}
 
 	/**
 	 * reads all messaged according to the passed severity from the status message list and appends them to
 	 * the passed toolTipText
 	 * 
-	 * @param String ToolTipText where message must be appended to
-	 * @param int severity of messages to be appended
-	 * @param int maximum number of messages to be appended
+	 * @param toolTipTextList where message must be appended to
+	 * @param severity of messages to be appended
+	 * @param maxMessages maximum number of messages to be appended
 	 * 
-	 * @return String toolTipText
 	 */
-	protected List appendToolTipTextList(List toolTipTextList, int severity, int maxMessages) {
+	protected void appendToolTipTextList(List<String> toolTipTextList, int severity, int maxMessages) {
 
-		List newToolTipTextList = toolTipTextList;
-
-		if (newToolTipTextList.size() < maxMessages) {
-			List messageList = getMessageList(severity);
-
-			//
-			// get all messages
-			//
-			for (int i = 0;(i < messageList.size()) && (newToolTipTextList.size() < maxMessages); i++) {
-				if (newToolTipTextList != null) {
-					newToolTipTextList.add(messageList.get(i));
-				}
+		for(String m : getMessageList(severity) ) {
+			if (toolTipTextList.size() < maxMessages) {
+				toolTipTextList.add(m);
+			} else {
+				break;
 			}
 		}
-
-		return newToolTipTextList;
 	}
 
 	/**
 	 * returns messages according to the passed severity
 	 * 
-	 * @param int severity of messages to be returned
-	 * 
+	 * @param severity of messages to be returned 
 	 * @return List of messages according to the passed severity
 	 * 
 	 */
-	protected List getMessageList(int severity) {
-		List messageList = new ArrayList();
+	
+	protected List<String> getMessageList (int severity) {
+		List<String> messageList = new ArrayList<String>();
 
-		for (int i = 0; i < getStatusMessageList().size(); i++) {
-			StatusMessage statusMessage = (StatusMessage) getStatusMessageList().get(i);
-
-			if (statusMessage.getSeverity() == severity) {
-				messageList.add(statusMessage.getMessage());
+		for (StatusMessage m : statusMessageList ) {
+			if (m.getSeverity() == severity) {
+				messageList.add(m.getMessage());
 			}
 		}
-
 		return messageList;
 	}
 
@@ -456,28 +435,25 @@ public class StatusLabel2 {
 	 */
 	protected int getHighestSeverity() {
 		int severity = IStatus.OK;
-
-		for (int i = 0; i < getStatusMessageList().size(); i++) {
-			StatusMessage statusMessage = (StatusMessage) getStatusMessageList().get(i);
-			if (statusMessage.getSeverity() > severity) {
-				severity = statusMessage.getSeverity();
-			}
+		for(StatusMessage m : statusMessageList) {
+			if (m.getSeverity() > severity) {
+				severity = m.getSeverity();
+			}			
 		}
-
 		return severity;
 	}
 
 	/**
 	 * @return List of status messages
 	 */
-	protected List getStatusMessageList() {
+	protected List<StatusMessage> getStatusMessageList() {
 		return statusMessageList;
 	}
 
 	/**
 	 * @param List of status messages
 	 */
-	protected void setStatusMessageList(List list) {
+	protected void setStatusMessageList(List<StatusMessage> list) {
 		statusMessageList = list;
 	}
 }
