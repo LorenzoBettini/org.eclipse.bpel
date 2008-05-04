fe6f50d0a78c465c7b62f011ed46375a339422fe
odanilov <odanilov>
2008-05-04 11:05:32 +0000

    eliminated warnings

--- plugins/org.eclipse.bpel.wsil.model/src/org/eclipse/bpel/wsil/model/Activator.java
+++ plugins/org.eclipse.bpel.wsil.model/src/org/eclipse/bpel/wsil/model/Activator.java
@@ -25,6 +25,7 @@ public class Activator extends Plugin {
 	 * (non-Javadoc)
 	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 	}
@@ -33,6 +34,7 @@ public class Activator extends Plugin {
 	 * (non-Javadoc)
 	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
