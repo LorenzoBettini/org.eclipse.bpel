9f4e402100c11dc0c0eb97eaa80f701d3e7bef49
rbrodt <rbrodt>
2010-09-09 21:14:15 +0000

    Bug 118505 - hack to make the org.eclipse.bpel.examples.extensionPoints example compile.

--- plugins/org.eclipse.bpel.examples.extensionPoints/src/org/eclipse/bpel/examples/extensionpoints/expression/editors/JavaScriptTextEditor.java
+++ plugins/org.eclipse.bpel.examples.extensionPoints/src/org/eclipse/bpel/examples/extensionpoints/expression/editors/JavaScriptTextEditor.java
@@ -18,50 +18,54 @@ import org.eclipse.ui.texteditor.ITextEditorActionConstants;
  * @author Michal Chmielewski (michal.chmielewski@oracle.com)
  * @date Jul 17, 2007
  *
+ * NOTE: This needs to be reworked to use the WST JSDT toolkit. Also, the org.eclipse.bpel.common.ui.embeddedEditors
+ * extension point has not yet been fully defined in the documentation (nor can I find any references in
+ * the code to where an embedded editor is actually being instantiated, with the exception of expression editors,
+ * which are defined with the org.eclipse.bpel.ui.expressionEditors extension point!)
  */
 @SuppressWarnings("nls")
-public class JavaScriptTextEditor extends org.eclipse.wst.javascript.ui.internal.editor.JSEditor {
+public class JavaScriptTextEditor /* extends org.eclipse.wst.javascript.ui.internal.editor.JSEditor */ {
 	
 	/** The Editor ID */
 	public static final String TEXT_EDITOR_ID = "org.eclipse.bpel.examples.extensionPoints.javaScriptEditor";
 
-	/**
-	 * 
-	 */
-	public JavaScriptTextEditor() {
-		super();
-		setDocumentProvider(new TextDocumentProvider());
-		
-	}
-
-	/**
-	 * Remove unnecessary actions.
-	 */
-	@Override
-	protected void createActions() {
-		super.createActions();
-		setAction(ITextEditorActionConstants.PRINT, null);
-		setAction(ITextEditorActionConstants.SAVE, null);
-		setAction(ITextEditorActionConstants.REVERT_TO_SAVED, null);
-	}
-
-	/**
-	 * Returns the current contents of this editor.
-	 * @return the contents of the editor
-	 */
-	
-	public String getContents() {
-		IEditorInput input = getEditorInput();
-		IDocument doc = getDocumentProvider().getDocument(input);
-		return doc.get();
-	}
-
-	/**
-	 * Removes the dirty state.
-	 */
-	public void markAsClean() {
-		// this is almost a no-op
-		doSave(null);
-	}
+//	/**
+//	 * 
+//	 */
+//	public JavaScriptTextEditor() {
+//		super();
+//		setDocumentProvider(new TextDocumentProvider());
+//		
+//	}
+//
+//	/**
+//	 * Remove unnecessary actions.
+//	 */
+//	@Override
+//	protected void createActions() {
+//		super.createActions();
+//		setAction(ITextEditorActionConstants.PRINT, null);
+//		setAction(ITextEditorActionConstants.SAVE, null);
+//		setAction(ITextEditorActionConstants.REVERT_TO_SAVED, null);
+//	}
+//
+//	/**
+//	 * Returns the current contents of this editor.
+//	 * @return the contents of the editor
+//	 */
+//	
+//	public String getContents() {
+//		IEditorInput input = getEditorInput();
+//		IDocument doc = getDocumentProvider().getDocument(input);
+//		return doc.get();
+//	}
+//
+//	/**
+//	 * Removes the dirty state.
+//	 */
+//	public void markAsClean() {
+//		// this is almost a no-op
+//		doSave(null);
+//	}
 	
 }
