bc09daf1bd1bf5be14934c6fecf40add70f994e1
rbrodt <rbrodt>
2010-10-13 11:02:19 +0000

    bug 327644

--- plugins/org.eclipse.bpel.ui/src/org/eclipse/bpel/ui/editparts/LinkEditPart.java
+++ plugins/org.eclipse.bpel.ui/src/org/eclipse/bpel/ui/editparts/LinkEditPart.java
@@ -385,7 +385,9 @@ public class LinkEditPart extends AbstractConnectionEditPart {
 				if (labeledElement != null) {
 					childType = labeledElement.getTypeLabel(thisPart.getModel());
 					displayName = labeledElement.getLabel(thisPart.getModel());
-					if (childType != null && displayName.equals(childType)) {
+					// bug 327644
+					// prevent possible NPE
+					if (childType != null && childType.equals(displayName)) {
 						childType = null;
 					}
 				} else {
