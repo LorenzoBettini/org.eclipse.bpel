7abfbf82909b18a19a2e93537e5de67a9ee01e74
rbrodt <rbrodt>
2010-11-17 22:23:45 +0000

    Bugzilla 330527

--- plugins/org.eclipse.bpel.common.ui/src/org/eclipse/bpel/common/ui/ResourceMarkerDecorator.java
+++ plugins/org.eclipse.bpel.common.ui/src/org/eclipse/bpel/common/ui/ResourceMarkerDecorator.java
@@ -4,20 +4,12 @@
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
  */
 package org.eclipse.bpel.common.ui;
 
