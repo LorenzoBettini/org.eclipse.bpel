44110c58b80e0ad6cead47cfaa07ea53549d2bbd
rbrodt <rbrodt>
2010-11-22 19:48:29 +0000

    https://bugs.eclipse.org/bugs/show_bug.cgi?id=330813 Sync-up from JBoss bug fixes

--- plugins/org.eclipse.bpel.common.ui/src/org/eclipse/bpel/common/ui/ResourceMarkerDecorator.java
+++ plugins/org.eclipse.bpel.common.ui/src/org/eclipse/bpel/common/ui/ResourceMarkerDecorator.java
@@ -1,15 +1,9 @@
 /*
- * JBoss, Home of Professional Open Source
- * Copyright 2005, JBoss Inc., and individual contributors as indicated
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
- * which accompanies this distribution.
- * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at
- * http://www.eclipse.org/org/documents/edl-v10.php.
+ * Copyright (c) 2010 JBoss, Inc. and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  */
 package org.eclipse.bpel.common.ui;
 
@@ -31,17 +25,15 @@ import org.eclipse.jface.viewers.ILightweightLabelDecorator;
  */
 public class ResourceMarkerDecorator implements ILightweightLabelDecorator, ICommonUIConstants {
 
-	private static ImageDescriptor img_error;
-	private static ImageDescriptor img_warning;
-	private static ImageDescriptor img_info;
+	// https://bugs.eclipse.org/bugs/show_bug.cgi?id=330813
+	// Do not initialize these in constructor - BPEL plugin may not be available yet
+	private static ImageDescriptor img_error = null;
+	private static ImageDescriptor img_warning = null;
+	private static ImageDescriptor img_info = null;
 	
 	public ResourceMarkerDecorator()
 	{
 		super();
-		
-		img_error = CommonUIPlugin.getDefault().getImageRegistry().getDescriptor(ICommonUIConstants.ICON_ERROR);
-		img_warning = CommonUIPlugin.getDefault().getImageRegistry().getDescriptor(ICommonUIConstants.ICON_WARNING);
-		img_info = CommonUIPlugin.getDefault().getImageRegistry().getDescriptor(ICommonUIConstants.ICON_INFO);
 	}
 	
 	public void addListener(ILabelProviderListener listener) {
@@ -62,12 +54,18 @@ public class ResourceMarkerDecorator implements ILightweightLabelDecorator, ICom
 			int severity = ((IResource)element).findMaxProblemSeverity(IMarker.PROBLEM, true, IResource.DEPTH_ONE);
 			switch (severity) {
 			case IMarker.SEVERITY_ERROR:
+				if (img_error==null)
+					img_error = CommonUIPlugin.getDefault().getImageRegistry().getDescriptor(ICommonUIConstants.ICON_ERROR);
 				decoration.addOverlay(img_error);
 				break;
 			case IMarker.SEVERITY_WARNING:
+				if (img_warning==null)
+					img_warning = CommonUIPlugin.getDefault().getImageRegistry().getDescriptor(ICommonUIConstants.ICON_WARNING);
 				decoration.addOverlay(img_warning);
 				break;
 			case IMarker.SEVERITY_INFO:
+				if (img_info==null)
+					img_info = CommonUIPlugin.getDefault().getImageRegistry().getDescriptor(ICommonUIConstants.ICON_INFO);
 				decoration.addOverlay(img_info);
 				break;
 			}
