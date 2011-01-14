6b962ca619675bc605c00a132d36d3bc3f9ce4c4
rbrodt <rbrodt>
2011-01-14 21:40:38 +0000

    https://bugs.eclipse.org/bugs/show_bug.cgi?id=334424 Extension Activity deserializer needs to reuse the activity that was already created (if any).

--- examples/plugins/org.eclipse.bpel.extensionsample.model/src/org/eclipse/bpel/extensionsample/model/util/ExtensionSampleActivityDeserializer.java
+++ examples/plugins/org.eclipse.bpel.extensionsample.model/src/org/eclipse/bpel/extensionsample/model/util/ExtensionSampleActivityDeserializer.java
@@ -21,20 +21,27 @@ import org.w3c.dom.NodeList;
 public class ExtensionSampleActivityDeserializer implements BPELActivityDeserializer {
 
 	@Override
-	public Activity unmarshall(QName elementType, Node node, Process process, Map nsMap,
-			ExtensionRegistry extReg, URI uri, BPELReader bpelReader) {
+	public Activity unmarshall(QName elementType, Node node, Activity activity, Process process,
+			Map nsMap, ExtensionRegistry extReg, URI uri, BPELReader bpelReader) {
 
 		/*
 		 * SampleSimpleActivity
 		 */
 		if (ExtensionsampleConstants.ND_SAMPLE_SIMPLE_ACTIVITY.equals(elementType.getLocalPart())) {
 
-			// create a new SampleSimpleActivity model object
-			SampleSimpleActivity sampleSimpleActivity = ModelFactory.eINSTANCE
+			// create a new SampleSimpleActivity model object if not already created
+			SampleSimpleActivity sampleSimpleActivity;
+			// https://bugs.eclipse.org/bugs/show_bug.cgi?id=334424
+			if (activity instanceof SampleSimpleActivity) {
+				sampleSimpleActivity = (SampleSimpleActivity)activity;
+			}
+			else {
+				sampleSimpleActivity = ModelFactory.eINSTANCE
 					.createSampleSimpleActivity();
 
-			// attach the DOM node to our new activity
-			sampleSimpleActivity.setElement((Element) node);
+				// attach the DOM node to our new activity
+				sampleSimpleActivity.setElement((Element) node);
+			}
 
 			// handle the SampleExtensionAttribute
 			String attName = ModelPackage.eINSTANCE
@@ -56,11 +63,18 @@ public class ExtensionSampleActivityDeserializer implements BPELActivityDeserial
 			Element sampleStructuredActivityElement = (Element) node;
 
 			// create a new SampleStructuredActivity model object
-			SampleStructuredActivity sampleStructuredActivity = ModelFactory.eINSTANCE
+			SampleStructuredActivity sampleStructuredActivity;
+			if (activity instanceof SampleStructuredActivity) {
+				sampleStructuredActivity = (SampleStructuredActivity)activity;
+			}
+			else
+			{
+				sampleStructuredActivity = ModelFactory.eINSTANCE
 					.createSampleStructuredActivity();
-
-			// attach the DOM node to our new activity
-			sampleStructuredActivity.setElement(sampleStructuredActivityElement);
+				
+				// attach the DOM node to our new activity
+				sampleStructuredActivity.setElement(sampleStructuredActivityElement);
+			}
 
 			// handle the child activity
 			NodeList childElements = sampleStructuredActivityElement.getChildNodes();
@@ -71,9 +85,9 @@ public class ExtensionSampleActivityDeserializer implements BPELActivityDeserial
 					// the only element node is the child activity
 					if ((childElements.item(i).getNodeType() == Node.ELEMENT_NODE)) {
 						activityElement = (Element) childElements.item(i);
-						Activity activity = bpelReader.xml2Activity(activityElement);
-						if (activity != null) {
-							sampleStructuredActivity.setActivity(activity);
+						Activity childActivity = bpelReader.xml2Activity(activityElement);
+						if (childActivity != null) {
+							sampleStructuredActivity.setActivity(childActivity);
 						}
 					}
 				}
@@ -82,7 +96,7 @@ public class ExtensionSampleActivityDeserializer implements BPELActivityDeserial
 			return sampleStructuredActivity;
 		}
 
-		System.out.println("Cannot handle this kind of element");
+		System.err.println("Cannot handle this kind of element");
 		return null;
 	}
 
