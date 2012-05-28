224fe5c6461a41749e7fc42428c4f1f55ef6a18f
Lorenzo Bettini <bettini@dsi.unifi.it>
2012-05-28 12:28:22 +0200

    a simple xtext hello dsl

---
+++ xtext/org.xtext.example.mydsl.ui/src/org/xtext/example/mydsl/ui/XtextEmbeddedEditorProvider.java
@@ -0,0 +1,67 @@
+package org.xtext.example.mydsl.ui;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.resource.XtextResourceFactory;
+import org.eclipse.xtext.resource.XtextResourceSet;
+import org.eclipse.xtext.ui.editor.embedded.EmbeddedEditor;
+import org.eclipse.xtext.ui.editor.embedded.EmbeddedEditorFactory;
+import org.eclipse.xtext.ui.editor.embedded.EmbeddedEditorModelAccess;
+import org.eclipse.xtext.ui.editor.embedded.IEditedResourceProvider;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+public class XtextEmbeddedEditorProvider {
+
+	@Inject
+	protected EmbeddedEditorFactory editorFactory;
+
+	@Inject
+	private Provider<XtextResourceSet> resourceSetProvider;
+
+	@Inject
+	private XtextResourceFactory resourceFactory;
+
+	/**
+	 * The constructor.
+	 */
+	public XtextEmbeddedEditorProvider() {
+	}
+
+	public EmbeddedEditor getXtextEmbeddedEditor(Composite parent) {
+		IEditedResourceProvider resourceProvider = new IEditedResourceProvider() {
+
+			@Override
+			public XtextResource createResource() {
+				XtextResourceSet xtextResourceSet = resourceSetProvider.get();
+				Resource resource = resourceFactory
+						.createResource(computeUnusedUri(xtextResourceSet));
+				xtextResourceSet.getResources().add(resource);
+				return (XtextResource) resource;
+			}
+
+			protected URI computeUnusedUri(ResourceSet resourceSet) {
+				String name = "__synthetic";
+				for (int i = 0; i < Integer.MAX_VALUE; i++) {
+					URI syntheticUri = URI.createURI(name + i + ".mydsl");
+					if (resourceSet.getResource(syntheticUri, false) == null)
+						return syntheticUri;
+				}
+				throw new IllegalStateException();
+			}
+		};
+
+		EmbeddedEditor editor = editorFactory.newEditor(resourceProvider)
+				.showErrorAndWarningAnnotations().withParent(parent);
+
+		EmbeddedEditorModelAccess partialEditor = editor
+				.createPartialEditor(true);
+
+		return editor;
+	}
+
+}
