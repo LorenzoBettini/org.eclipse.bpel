c71058f24a8f134e73e88d1884aeeedfce6f0396
Lorenzo Bettini <bettini@dsi.unifi.it>
2012-10-17 08:56:20 +0200

    bug 391913: reintroduce expression editors https://bugs.eclipse.org/bugs/show_bug.cgi?id=391913
    Change-Id: I46ecdb09f03d33b0f33674295153e3249491d14f

--- plugins/org.eclipse.bpel.ui.noEmbeddedEditors/src/org/eclipse/bpel/ui/extensions/BPELUIRegistry.java
+++ plugins/org.eclipse.bpel.ui.noEmbeddedEditors/src/org/eclipse/bpel/ui/extensions/BPELUIRegistry.java
@@ -11,12 +11,14 @@
 package org.eclipse.bpel.ui.extensions;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.eclipse.bpel.ui.BPELUIPlugin;
 import org.eclipse.bpel.ui.IHoverHelper;
 import org.eclipse.bpel.ui.bpelactions.AbstractBPELAction;
 import org.eclipse.bpel.ui.factories.AbstractUIObjectFactory;
+import org.eclipse.bpel.ui.properties.IAssignCategory;
 import org.eclipse.bpel.ui.util.BPELUtil;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
@@ -38,6 +40,7 @@ public class BPELUIRegistry {
 	static final String ATT_CLASS = "class"; //$NON-NLS-1$
 	static final String ATT_LABEL = "label"; //$NON-NLS-1$
 	static final String EXTPT_ACTIONS = "actions"; //$NON-NLS-1$
+	static final String EXTPT_ASSIGN_CATEGORY_ADDITIONS = "assignCategoryAdditions"; //$NON-NLS-1$
 	static final String ELEMENT_CATEGORY = "category"; //$NON-NLS-1$
 	static final String ATT_NAME = "name"; //$NON-NLS-1$
 	static final String ATT_ID = "id"; //$NON-NLS-1$
@@ -234,6 +237,22 @@ public class BPELUIRegistry {
         this.uiObjectFactoryDescriptor = new UIObjectFactoryDescriptor[factories.size()];
         factories.toArray(this.uiObjectFactoryDescriptor);
     }
+
+    public List<IAssignCategory> getAdditionalAssignCategories() {
+		List<IAssignCategory> assignCategories = new LinkedList<IAssignCategory>();
+    	IConfigurationElement[] extensions = getConfigurationElements(EXTPT_ASSIGN_CATEGORY_ADDITIONS);
+        for( IConfigurationElement element : extensions ) {
+        	//String name = element.getAttribute(ATT_NAME);
+        	try {
+				IAssignCategory assignCategory = (IAssignCategory) element.createExecutableExtension(ATT_CLASS);
+				assignCategories.add(assignCategory);
+				System.out.println(assignCategory);
+			} catch (CoreException e1) {
+				e1.printStackTrace();
+			}
+        }
+		return assignCategories;
+	}
 
 	/**
 	 * Read all the model listeners
