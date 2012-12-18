cf85804cd634beb5a1d1899293b95f3300455303
Lorenzo Bettini <bettini@dsi.unifi.it>
2012-12-18 10:58:56 +0100

    bug 395840: Embeddable Xtext editors https://bugs.eclipse.org/bugs/show_bug.cgi?id=395840
    Change-Id: Ibf1da248db8328ab2a6fa0087bdf25f9ca1f54c7

---
+++ examples/plugins/org.eclipse.bpel.ui.xtext.examples/src/org/eclipse/bpel/ui/xtext/examples/FowlerDslXtextExpressionAssignCategory.java
@@ -0,0 +1,42 @@
+package org.eclipse.bpel.ui.xtext.examples;
+
+import org.eclipse.bpel.ui.properties.xtext.AbstractXtextExpressionAssignCategory;
+import org.eclipse.xtext.example.fowlerdsl.ui.internal.StatemachineActivator;
+
+import com.google.inject.Injector;
+
+/**
+ * @author Lorenzo Bettini
+ *
+ */
+public class FowlerDslXtextExpressionAssignCategory extends
+		AbstractXtextExpressionAssignCategory {
+
+//	public FowlerDslXtextExpressionAssignCategory(
+//			BPELPropertySection ownerSection) {
+//		super(ownerSection);
+//	}
+
+	@Override
+	public String getName() {
+		return "Xtext FowlerDsl Editor";
+	}
+
+	public Injector getInjector() {
+		return StatemachineActivator
+				.getInstance()
+				.getInjector(
+						StatemachineActivator.ORG_ECLIPSE_XTEXT_EXAMPLE_FOWLERDSL_STATEMACHINE);
+	}
+	
+	@Override
+	public String getExpressionLanguage() {
+		return "fowler";
+	}
+
+	@Override
+	protected String getStyledTextLabelString() {
+		return "Edit the statemachine expression";
+	}
+
+}
